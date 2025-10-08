04c9041e50e3f102e9ab1ba1c47fd052b169a04e
sudheer-tsi <Sudheer.Koyyana@tsico.com>
2025-10-08 13:04:33 +0530

    Committing 6 items from workspace 26dd097d-f293-420e-b5f2-5c949c38b071

---
+++ 01_bronze/01_raw/01_ingestion/02_notebooks/nb_ingestion.Notebook/notebook-content.py
@@ -0,0 +1,53 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6c97b5de-da3d-4c45-81d7-5250148c70e4",
+# META       "default_lakehouse_name": "edp_dev_raw",
+# META       "default_lakehouse_workspace_id": "26dd097d-f293-420e-b5f2-5c949c38b071",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6c97b5de-da3d-4c45-81d7-5250148c70e4"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# 
+# ##### **Author:** JMAN - Nithish
+# ##### **Tittle:** Ingesting the data from extracts into the delta tables
+
+# CELL ********************
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("ParquetToDelta").getOrCreate()
+
+parquet_path = "abfss://26dd097d-f293-420e-b5f2-5c949c38b071@onelake.dfs.fabric.microsoft.com/6c97b5de-da3d-4c45-81d7-5250148c70e4/Files/sample_parquet_testing"
+delta_path = "abfss://ws-edp-dev@onelake.dfs.fabric.microsoft.com/edp_dev_raw.Lakehouse/Tables/dbo/parquet_to_delta_test"
+
+df = spark.read.parquet(parquet_path)
+
+# Step 2: Drop the column if it exists
+if "loaded_at" in df.columns:
+    df = df.drop("loaded_at")
+
+# Step 3: Write into Delta
+df.write.format("delta").mode("overwrite").save(delta_path)
+    
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
